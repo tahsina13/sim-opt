c0203096b4c4e151e643ba7e56ca8bd590cdac1d
Darren Lo <darren.lo@stonybrook.edu>
2025-12-11 23:48:02 -0500

    Graph scales with cost for knapsack

--- src/visualizers/cost_visualizer.py
+++ src/visualizers/cost_visualizer.py
@@ -18,18 +18,18 @@ class CostVisualizer(Visualizer):
         self.ax = ax
         self.niter = niter
         self.optim = optim
+        self.line = None
 
     def setup(self):
         self.ax.set_title("Cost")
         self.ax.set_xlabel("iterations")
         self.ax.set_ylabel("cost")
         self.ax.set_xlim(0, self.niter)
-        order = 10 ** np.floor(np.log10(self.optim.solution.cost))
-        interval = order // 2
-        ylim = np.ceil((self.optim.solution.cost * 1.2) / order) * order
+        initial_cost = self.optim.solution.cost
+        order = 10 ** np.floor(np.log10(max(abs(initial_cost), 1)))
+        ylim = np.ceil((initial_cost * 1.2) / order) * order
         self.ax.set_ylim(0, ylim)
-        self.ax.set_yticks(np.linspace(0, ylim, int(ylim // interval) + 1))
-        (self.line,) = self.ax.plot(0, self.optim.solution.cost, c="blue")
+        (self.line,) = self.ax.plot(0, initial_cost, c="blue")
 
     def update(self, k: int):
         if self.line is None:
@@ -39,3 +39,11 @@ class CostVisualizer(Visualizer):
         xdata = np.append(self.line.get_xdata(), k)
         ydata = np.append(self.line.get_ydata(), self.optim.solution.cost)
         self.line.set_data(xdata, ydata)
+        current_cost = self.optim.solution.cost
+        _, current_ylim = self.ax.get_ylim()
+        if current_cost > current_ylim * 0.9:
+            order = 10 ** np.floor(np.log10(max(abs(current_cost), 1)))
+            new_ylim = np.ceil((current_cost * 1.2) / order) * order
+            self.ax.set_ylim(0, new_ylim)
+            interval = order // 2
+            self.ax.set_yticks(np.linspace(0, new_ylim, int(new_ylim // interval) + 1))
